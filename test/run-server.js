6f3056f669490b44447a02eee560c0e6066b9c76
杨森 <alex.yang@iymedia.net>
2019-07-29 11:22:42 +0800

    update

--- test/run-server.js
+++ test/run-server.js
@@ -2,5 +2,7 @@ const Server = require("../src/server/index");
 
 process.env.NODE_ENV = "production";
 
-const server = new Server();
+const server = new Server({
+    accessLog: true,
+});
 server.start();
